3bcb5279658ee5902bddc13cdf5d26930702e880
dd301 <donatodoley1@gmail.com>
2022-05-31 19:23:20 +0530

    9-11

---
+++ 9-11.py
@@ -0,0 +1,4 @@
+import user as u
+
+admin = u.Admin('Donato', 'Doley', '20', 'Male')
+admin.admin_privileges.show_privileges()
---
+++ user.py
@@ -0,0 +1,36 @@
+class User():
+    def __init__(self, first_name, last_name, age, 
+            sex):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.sex = sex
+    
+    def describe_user(self):
+        print("The users name is " + self.first_name 
+        + " " + self.last_name + ". Their age is " + self.age +
+        " and their sex is " + self.sex + ".")
+    
+    def greet_user(self):
+        print("Hello, " + self.first_name + " " +
+        self.last_name + "!")
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, 
+            sex):
+        super().__init__(first_name, last_name, age,
+                        sex)
+
+        self.admin_privileges = Privileges()
+
+
+class Privileges():
+    def __init__(self):
+         self.privileges = ['can add post', 
+                        'can delete post',
+                        'can ban user']
+    
+    def show_privileges(self):
+        print("An admins privileges are:")
+        for privilege in self.privileges:
+            print(privilege)
