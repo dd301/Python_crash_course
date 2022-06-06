676f1a17bf1ea54817c0fb1064df0f7c7c455648
dd301 <donatodoley1@gmail.com>
2022-06-06 14:02:57 +0530

    added 10-12

---
+++ 10/10-12.py
@@ -0,0 +1,12 @@
+import json
+
+filename = 'fav_num.txt'
+
+try:
+    with open(filename) as f_obj:
+        num = json.load(f_obj)
+        print("I know your favourite number! It's " + num)
+except FileNotFoundError:
+    fav_num = input("What is your favourite number? ")
+    with open(filename, 'w') as f_obj:
+        json.dump(fav_num, f_obj)
